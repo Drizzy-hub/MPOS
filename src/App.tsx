43d3e8ee7b1a373db576c97381980a05193c8dc5
Drizzy <66376866+Drizzy-hub@users.noreply.github.com>
2024-06-09 23:11:10 +0100

    chore: retrieve from local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// import React, { useContext } from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import { AppNavigator, AuthNavigator } from "./navigation";
-import { Email, Landing, Login, MissingPage, Signup } from './pages';
-// import { AuthenticatedUserContext } from "./provider";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Email, Landing, MissingPage } from './pages';
 import './App.css';
+import { AppNavigator, AuthNavigator } from './navigation';
+
 const formData = {
 	email: '',
 	password: '',
@@ -16,29 +16,37 @@ const formData = {
 	state: '',
 	country: '',
 };
+
 function App() {
-	// const { user } = useContext(AuthenticatedUserContext);
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		// Retrieve user from local storage
+		const storedUser = localStorage.getItem('user');
+		if (storedUser) {
+			setUser(JSON.parse(storedUser));
+		}
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route key={'LandingPage'} path={'/'} element={<Landing />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
 				<Route path="/verify-email" element={<Email formData={formData} />} />
-				{/* {AuthNavigator.map(({ path, Component, name }) => (
-          <Route 
-            key={name} 
-            path={path} 
-            element={!user ? <Component /> : <Navigate to="/dashboard" />} 
-          />
-        ))}
-        {AppNavigator.map(({ path, Component, name }) => (
-          <Route 
-            key={name} 
-            path={path} 
-            element={user ? <Component /> : <Navigate to="/login" />} 
-          />
-        ))} */}
+				{AuthNavigator.map(({ path, Component, name }) => (
+					<Route
+						key={name}
+						path={path}
+						element={!user ? <Component /> : <Navigate to="/dashboard" />}
+					/>
+				))}
+				{AppNavigator.map(({ path, Component, name }) => (
+					<Route
+						key={name}
+						path={path}
+						element={user ? <Component /> : <Navigate to="/login" />}
+					/>
+				))}
 				<Route path="*" element={<MissingPage />} />
 			</Routes>
 		</BrowserRouter>
